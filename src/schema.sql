ac9e72cb22ee5593b89880b05bcfa9570644cfa2
Griffanian <milesbloom526@gmail.com>
2025-09-04 17:15:31 +0100

    got all working except for duplicates

--- src/schema.sql
+++ src/schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS contacts (
     phone_number TEXT,
     email TEXT,
     name TEXT,
-    notes TEXT
+    notes TEXT,
+    UNIQUE(branch_id, phone_number, email, name)
 );
 
 CREATE TABLE IF NOT EXISTS specialism_types (
@@ -30,5 +31,6 @@ CREATE TABLE IF NOT EXISTS branch_specialisms (
     branch_id INTEGER NOT NULL REFERENCES branches(branch_id) ON DELETE CASCADE,
     specialism_id INTEGER NOT NULL REFERENCES specialism_types(specialism_id) ON DELETE CASCADE,
     notes TEXT,
-    PRIMARY KEY (branch_id, specialism_id)
+    PRIMARY KEY (branch_id, specialism_id),
+    UNIQUE(branch_id, specialism_id)
 );
